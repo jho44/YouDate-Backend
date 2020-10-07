38ae820459b03bfda85eed0ee5a0aa1d4a4d44b5
Jessica Ho <jessicaho@jessicas-mbp-3.attlocal.net>
2020-10-07 00:08:33 -0700

    index to model

--- user_model.js
+++ user_model.js
@@ -9,6 +9,8 @@ const Pool = require("pg").Pool;
 //   password = url[6],
 //   port = url[10];
 
+console.log("url: ", process.env.DATABASE_URL);
+
 const pool = new Pool({
   // user: user,
   // host: host,
@@ -22,17 +24,47 @@ const pool = new Pool({
 });
 
 // this function's just for testing
-const getUsers = () => {
-  return new Promise(function (resolve, reject) {
-    pool.query("SELECT * FROM youdate ORDER BY id ASC;", (error, results) => {
-      if (error) reject(error);
-      resolve(results.rows);
+const getUsers = async () => {
+  {
+    /*
+      return new Promise(function (resolve, reject) {
+      pool.query("SELECT * FROM youdate ORDER BY id ASC;", (error, results) => {
+        if (error) reject(error);
+        resolve(results.rows);
+      });
     });
-  });
+  */
+  }
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM youdate;");
+    client.release();
+
+    return result ? result.rows : null;
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
 };
 
 // get one user by email
-const getUser = (headers) => {
+const getUser = async (headers) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query(
+      "SELECT * FROM youdate WHERE email = $1;",
+      [headers.email]
+    );
+    client.release();
+    console.log("result: ", result);
+    if (result.rowCount) return true;
+    return false;
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+  {
+    /* 
   const resBoi = new Promise(function (resolve, reject) {
     pool.query(
       "SELECT * FROM youdate WHERE email = $1;",
@@ -51,21 +83,42 @@ const getUser = (headers) => {
       }
     );
   });
+  */
+  }
 };
 
-const insertUser = (body) => {
-  return new Promise(function (resolve, reject) {
+const insertUser = async (body) => {
+  try {
+    const client = await pool.connect();
     const { name, age, email, photo, subscriptions, description } = body;
-
-    pool.query(
+    const result = await client.query(
       "INSERT INTO youdate (name, age, email, photo, accept_list, reject_list, subscriptions, description) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
-      [name, age, email, photo, "{}", "{}", subscriptions, description],
-      (error, results) => {
-        if (error) reject(error);
-        resolve(`A new user has been added: ${results.rows[0]}`);
-      }
+      [name, age, email, photo, "{}", "{}", subscriptions, description]
     );
-  });
+    client.release();
+
+    return result ? result.rows : null;
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+
+  {
+    /*
+      return new Promise(function (resolve, reject) {
+      const { name, age, email, photo, subscriptions, description } = body;
+
+      pool.query(
+        "INSERT INTO youdate (name, age, email, photo, accept_list, reject_list, subscriptions, description) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
+        [name, age, email, photo, "{}", "{}", subscriptions, description],
+        (error, results) => {
+          if (error) reject(error);
+          resolve(`A new user has been added: ${results.rows[0]}`);
+        }
+      );
+    });
+  */
+  }
 };
 
 /* TODO: delete user */
